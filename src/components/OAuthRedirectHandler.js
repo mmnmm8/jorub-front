ae939fb2fdeeba89322b751f8c59b95ba0f096c9
Chaewon <lcw3714@naver.com>
2023-02-12 13:46:33 +0900

    카카오 소셜 로그인 토큰 받아오는 부분 수정

--- src/components/OAuthRedirectHandler.js
+++ src/components/OAuthRedirectHandler.js
@@ -9,12 +9,13 @@ function OAuthRedirectHandler() {
   useEffect(() => {
     (async () => {
       try {
-        const res = await axios.get(`api/code=${code}`);
+        const res = await axios.get(`http://localhost:8080/api?code=${code}`);
         const token = res.headers.authorization;
         window.localStorage.setItem('token', token);
         navigate('/clubs');
       } catch (e) {
-        console.error(e);
+        console.log('소셜로그인 에러', e);
+        window.alert('로그인에 실패하였습니다');
         navigate('/login');
       }
     })();
